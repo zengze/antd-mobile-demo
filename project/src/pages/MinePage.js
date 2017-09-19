6319d82026d8b5b394b8da4a3adadde4799d92cc
zengze <zengze@smart>
2017-09-19 17:00:30 +0800

    调整首页和我布局

--- project/src/pages/MinePage.js
+++ project/src/pages/MinePage.js
@@ -7,6 +7,8 @@ import {
   View,
 } from 'react-native';
 
+const marginTop = Platform.OS === 'ios' ? 20 : 0;
+
 export default class MinePage extends Component {
 
   _skip() {
@@ -15,19 +17,29 @@ export default class MinePage extends Component {
 
   render() {
 
-    let marginTop = Platform.OS === 'ios' ? 20 : 0;
-
     return(
-      <View style={{ flex: 1, marginTop: marginTop }}>
+      <View style={styles.container}>
         <Text onPress={this._skip.bind(this)}>返回上一界面</Text>
-        <Text style={{ textAlign: 'center' }}>
-          我是我
-        </Text>
+        <View style={styles.context}>
+          <Text>
+            我是我
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginTop: marginTop,
+  },
+  context: {
+    flex: 1,
+    // 设置主轴对齐方式
+    justifyContent: 'center',
+    // 设置侧轴对齐方式
+    alignItems: 'center',
+  }
 });
